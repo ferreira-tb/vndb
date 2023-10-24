9ccd9e384de4ea1144b50f4e223c2aa3dfd8f64b
Andrew Ferreira <andrew.shien2@gmail.com>
2023-10-24 12:13:46 -0300

    docs: add fiddle

---
+++ docs/components/JSFiddle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <iframe
+        width="100%"
+        height="500"
+        src="//jsfiddle.net/ferreiratb/0Lezvkfa/21/embedded/result,html,css/dark/"
+        frameborder="0"
+    >
+    </iframe>
+</template>
