2f59ec6482257b29c713ca96f901569cce627160
Andrew Ferreira <andrew.shien2@gmail.com>
2023-10-24 14:50:18 -0300

    refactor: change to javascript private fields

--- src/query.ts
+++ src/query.ts
@@ -2,14 +2,15 @@ import type {
     QueryBuilderBase,
     QueryBuilderFilter,
     QueryBuilderOptions,
-    QueryBuilderEndpoint
+    QueryBuilderEndpoint,
+    QueryBuilderInternalPush
 } from '../typings';
 
 export class QueryBuilder<T extends QueryBuilderEndpoint> implements QueryBuilderBase<T> {
-    private readonly filters: any[] = [];
-    private readonly indexMap: IndexMap;
-    
-    private readonly operator: QueryBuilderOperator<T>;
+    readonly #filters: any[] = [];
+    readonly #indexMap: IndexMap;
+    readonly #operator: QueryBuilderOperator<T>;
+
     public readonly compactFilters: string | null;
     public readonly options: Omit<QueryBuilderOptions<T>, 'filters'>;
 
@@ -22,75 +23,83 @@ export class QueryBuilder<T extends QueryBuilderEndpoint> implements QueryBuilde
         } else {
             this.compactFilters = null;
             if (Array.isArray(filters) && filters.length > 0) {
-                this.filters.push(...filters);
+                this.#filters.push(...filters);
             }
         }
 
-        this.indexMap = new IndexMap([[0, this.filters.length]]);
-        this.operator = new QueryBuilderOperator(this, this.push.bind(this));
+        this.#indexMap = new IndexMap([[0, this.#filters.length]]);
+        this.#operator = new QueryBuilderOperator(this, this.#push.bind(this));
     }
 
-    public readonly and:(cb: (builder: QueryBuilderBase<T>) => void) => QueryBuilder<T> = this.proxify(this._and);
-    public readonly or: (cb: (builder: QueryBuilderBase<T>) => void) => QueryBuilder<T> = this.proxify(this._or);
-    public readonly f: (name: QueryBuilderFilter<T>) => QueryBuilderOperator<T> = this.proxify(this._filter);
-    public readonly filter: (name: QueryBuilderFilter<T>) => QueryBuilderOperator<T> = this.proxify(this._filter);
-    public readonly v: (value: any) => QueryBuilder<T> = this.proxify(this._value);
-    public readonly value: (value: any) => QueryBuilder<T> = this.proxify(this._value);
+    public readonly and:(cb: (builder: QueryBuilderBase<T>) => void) => QueryBuilder<T> = this.#proxify(this.#and);
+    public readonly or: (cb: (builder: QueryBuilderBase<T>) => void) => QueryBuilder<T> = this.#proxify(this.#or);
+    public readonly f: (name: QueryBuilderFilter<T>) => QueryBuilderOperator<T> = this.#proxify(this.#filter);
+    public readonly filter: (name: QueryBuilderFilter<T>) => QueryBuilderOperator<T> = this.#proxify(this.#filter);
+    public readonly v: (value: any) => QueryBuilder<T> = this.#proxify(this.#value);
+    public readonly value: (value: any) => QueryBuilder<T> = this.#proxify(this.#value);
 
-    private readonly logicalCallback: QueryBuilderBase<T> = {
-        and: this._and.bind(this),
-        or: this._or.bind(this),
-        f: this._filter.bind(this),
-        filter: this._filter.bind(this),
-        v: this._value.bind(this),
-        value: this._value.bind(this)
+    public toArray(): any[] {
+        return JSON.parse(this.toJSON(0));
     }
 
-    private _and(cb: (builder: QueryBuilderBase<T>) => void) {
-        this.pushLogicalOperator('and');
-        cb(this.logicalCallback);
-        this.reduceDepth();
+    public toJSON(space: string | number = 4) {
+        return JSON.stringify(this.#sanitizeFilters(), null, space);
+    }
+
+    readonly #logicalCallback: QueryBuilderBase<T> = {
+        and: this.#and.bind(this),
+        or: this.#or.bind(this),
+        f: this.#filter.bind(this),
+        filter: this.#filter.bind(this),
+        v: this.#value.bind(this),
+        value: this.#value.bind(this)
+    }
+
+    #and(cb: (builder: QueryBuilderBase<T>) => void) {
+        this.#pushLogicalOperator('and');
+        cb(this.#logicalCallback);
+        this.#reduceDepth();
         return this;
     }
 
-    private _filter(name: QueryBuilderFilter<T>) {
+    #filter(name: QueryBuilderFilter<T>) {
         // Filters always start a new block.
         // Therefore, the value of the next index will always be 1.
         // e.g. ["lang", "=", "en"]
-        this.push([name], (d) => d + 1, () => 1);
-        return this.operator;
+        this.#push([name], (d) => d + 1, () => 1);
+        return this.#operator;
     }
 
-    private _or(cb: (builder: QueryBuilderBase<T>) => void) {
-        this.pushLogicalOperator('or');
-        cb(this.logicalCallback);
-        this.reduceDepth();
+    #or(cb: (builder: QueryBuilderBase<T>) => void) {
+        this.#pushLogicalOperator('or');
+        cb(this.#logicalCallback);
+        this.#reduceDepth();
         return this;
     }
 
-    private _value(value: any) {
+    #value(value: any) {
         // Values follow operators.
         // They will close the current block and decrease the depth level.
-        const depth = this.indexMap.depth();
-        const parentIndex = this.indexMap.get(depth - 1);
-        this.push(value, (d) => d - 1, () => parentIndex + 1);
+        const depth = this.#indexMap.depth();
+        const parentIndex = this.#indexMap.get(depth - 1);
+        this.#push(value, (d) => d - 1, () => parentIndex + 1);
         return this;
     }
 
-    private goToCurrentDepth() {
-        const keys = Array.from(this.indexMap.keys()).sort((a, b) => a - b);
+    #goToCurrentDepth() {
+        const keys = Array.from(this.#indexMap.keys()).sort((a, b) => a - b);
         keys.pop();
 
-        let currentArray = this.filters;
+        let currentArray = this.#filters;
         for (const key of keys) {
-            const arrayIndex = this.indexMap.get(key);
+            const arrayIndex = this.#indexMap.get(key);
             currentArray = currentArray[arrayIndex];
         }
 
         return currentArray;
     }
 
-    private proxify<T extends (...args: any[]) => any>(fn: T) {
+    #proxify<T extends (...args: any[]) => any>(fn: T) {
         return new Proxy(fn.bind(this), {
             apply: (target, thisArg, argumentsList) => {
                 if (this.compactFilters) {
@@ -101,52 +110,52 @@ export class QueryBuilder<T extends QueryBuilderEndpoint> implements QueryBuilde
         })
     }
 
-    private push(
+    #push(
         value: any,
         depthFn: ((depth: number) => number) | null,
         indexFn: ((index: number) => number) | null
     ) {
-        const depth = this.indexMap.depth();
-        const index = this.indexMap.index();
-        const array = this.goToCurrentDepth();
+        const depth = this.#indexMap.depth();
+        const index = this.#indexMap.index();
+        const array = this.#goToCurrentDepth();
         array[index] = value;
 
-        this.setIndex(depthFn, indexFn, depth, index);
+        this.#setIndex(depthFn, indexFn, depth, index);
     }
 
-    private pushLogicalOperator(operator: 'and' | 'or') {
+    #pushLogicalOperator(operator: 'and' | 'or') {
         // This kind of operator normally starts a new block by inserting itself at the beginning of it afterwards.
         // However, if the root array is empty, it should not start a block.
         // The depth must not increase in such cases, but the index should.
-        const op = this.filters.length === 0 ? operator : [operator];
-        this.push(
+        const op = this.#filters.length === 0 ? operator : [operator];
+        this.#push(
             op,
             (d) => Array.isArray(op) ? d + 1 : d,
             (i) => Array.isArray(op) ? 1 : i + 1
         );
     }
 
-    private reduceDepth() {
-        const depth = this.indexMap.depth();
+    #reduceDepth() {
+        const depth = this.#indexMap.depth();
         const reduceBy = depth === 0 ? 0 : depth - 1;
-        const parentIndex = this.indexMap.get(reduceBy);
-        this.setIndex((d) => d - 1, () => parentIndex + 1);
+        const parentIndex = this.#indexMap.get(reduceBy);
+        this.#setIndex((d) => d - 1, () => parentIndex + 1);
     }
 
-    private setIndex(
+    #setIndex(
         depthFn: ((depth: number) => number) | null,
         indexFn: ((index: number) => number) | null,
-        depth = this.indexMap.depth(),
-        index = this.indexMap.index()
+        depth = this.#indexMap.depth(),
+        index = this.#indexMap.index()
     ) {
-        this.indexMap.set(
+        this.#indexMap.set(
             depthFn ? depthFn(depth) : depth,
             indexFn ? indexFn(index) : index
         );
     }
 
-    private sanitizeFilters() {
-        let filters = this.filters;
+    #sanitizeFilters() {
+        let filters = this.#filters;
         if (filters.length === 1 && Array.isArray(filters[0])) {
             filters = filters[0];
         }
@@ -156,14 +165,6 @@ export class QueryBuilder<T extends QueryBuilderEndpoint> implements QueryBuilde
         }
 
         return filters;
-    }
-
-    public toArray(): any[] {
-        return JSON.parse(this.toJSON(0));
-    }
-
-    public toJSON(space: string | number = 4) {
-        return JSON.stringify(this.sanitizeFilters(), null, space);
     }
 }
 
@@ -180,17 +181,17 @@ class QueryBuilderOperator<T extends QueryBuilderEndpoint> {
     declare public readonly lte: QueryBuilder<T>;
     declare public readonly lowerOrEqual: QueryBuilder<T>;
     
-    constructor(builder: QueryBuilder<T>, push: QueryBuilder<T>['push']) {
+    constructor(builder: QueryBuilder<T>, push: QueryBuilderInternalPush) {
         // Operators follow a filter.
         return new Proxy(this, {
             get: (_target, key) => {
-                push(this.parse(key), null, (i) => i + 1);
+                push(this.#parse(key), null, (i) => i + 1);
                 return builder;
             }
         });
     }
 
-    private parse(key: string | symbol) {
+    #parse(key: string | symbol) {
         switch (key) {
             case 'eq': return '=';
             case 'equal': return '=';
