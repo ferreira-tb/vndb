4dc358df88bc5a2ff1d356791b95145fca30e18a
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-20 10:33:11 -0300

    chore: recreate component

---
+++ docs/components/JsFiddle.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  width?: string;
+  height?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  width: '60%',
+  height: '400px'
+});
+</script>
+
+<template>
+  <div :class="$style['js-fiddle']">
+    <iframe
+      src="//jsfiddle.net/ferreiratb/0Lezvkfa/27/embedded/result,js,html,css/dark/"
+      :width="width"
+      :height="height"
+      frameborder="0"
+    >
+    </iframe>
+  </div>
+</template>
+
+<style module>
+.js-fiddle {
+  display: flex;
+  justify-content: center;
+}
+</style>
